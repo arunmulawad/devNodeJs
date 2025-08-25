f2b28e3de633e79d101d2eb2042c15b7d2d985ca
arun mulawad <mulawadarun999@gmail.com>
2025-08-25 20:25:40 +0530

    routes

--- src/app.js
+++ src/app.js
@@ -2,12 +2,17 @@ const express = require("express")
 const app= express()
 
 app.use("/login",(req,res)=>{
-    res.send("welecome to servers")
+    res.send("welcome to servers")
+})
+app.use("/home/user",(req,res)=>{
+    res.send("welcome to User")
 })
 app.use("/home",(req,res)=>{
-    res.send("welecome to Dashboard")
+    res.send("welcome to Dashboard")
 })
-
+app.use("/",(req,res)=>{
+    res.send("welcome to Default")
+})
 app.listen(4000,()=>{
     console.log("server running successfully at Port 4000")
 })
