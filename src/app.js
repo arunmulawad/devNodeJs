8cb7c362da5bbc94abd04196e6e5f941b2efe926
arun mulawad <mulawadarun999@gmail.com>
2025-08-26 13:38:07 +0530

    nextHandler

--- src/app.js
+++ src/app.js
@@ -1,14 +1,27 @@
 const express = require("express")
 const app = express()
 
-app.get("/home/:userId/:name/:pass", (req, res) => {
-    const { userId, name, pass } = req.params
-    const { product } = req.query
-    console.log({ userId, name, pass, product })
-    res.send({ name: "arun" })
+app.get("/home", [(req, res, next) => {
+    console.log("1")
+    // res.send("welcome  1")
+    next()
+}, (req, res, next) => {
+    console.log("2")
+    // res.send("welcome  2")
+    next()
+}], [(req, res, next) => {
 
+    console.log("3")
+    next()
+    // res.send("welcome  31 ")
+
+}]
+)
+app.get("/home", (req, res, next) => {
+    console.log("welcome  outside")
+    res.send("welcome  outside")
+    next()
 })
-
 app.listen(4000, () => {
     console.log("server running successfully at Port 4000")
 })
